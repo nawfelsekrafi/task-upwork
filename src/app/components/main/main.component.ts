628a306f677f1a6a33e81231321dff77c7b8b766
nawfelsekrafi <nawfelsekrafi@gmail.com>
2021-12-01 20:17:08 +0000

    update every thing

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -8,7 +8,72 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
 
   constructor() { }
-
+  modeles: any[] = [
+    { 
+      isActive: true,
+      isChecked: true,
+      id: 1,
+      title: 'Pré-évaluation 1',
+      date: '31/07/2021 a 21:34'
+    },
+    { 
+      isActive: true,
+      isChecked: true,
+      id: 2,
+      title: 'Pré-évaluation 2',
+      date: '31/07/2021 a 21:34'
+    },
+    {
+      isActive: false,
+      isChecked: true,
+      id: 3,
+      title: 'Pré-évaluation 3',
+      date: '31/07/2021 a 21:34'
+    },
+    {
+      isActive: true,
+      isChecked: false,
+      id: 4,
+      title: 'Pré-évaluation 4',
+      date: '31/07/2021 a 21:34'
+    },
+    {
+      isActive: true,
+      isChecked: true,
+      id: 5,
+      title: 'Pré-évaluation 5',
+      date: '31/07/2021 a 21:34'
+    },
+    {
+      isActive: true,
+      isChecked: true,
+      id: 6,
+      title: 'Pré-évaluation 6',
+      date: '31/07/2021 a 21:34'
+    },
+    {
+      isActive: true,
+      isChecked: false,
+      id: 7,
+      title: 'Pré-évaluation 7',
+      date: '31/07/2021 a 21:34'
+    },
+    {
+      isActive: true,
+      isChecked: true,
+      id: 8,
+      title: 'Pré-évaluation 8',
+      date: '31/07/2021 a 21:34'
+    },
+    {
+      isActive: true,
+      isChecked: false,
+      id: 9,
+      title: 'Pré-évaluation 9',
+      date: '31/07/2021 a 21:34'
+    }
+    
+  ] ;
   ngOnInit(): void {
   }
 
