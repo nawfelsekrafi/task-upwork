54c31d63bfc0a3fed98db71007f3a9869e67bbcc
nawfelsekrafi <nawfelsekrafi@gmail.com>
2021-12-02 19:58:57 +0000

    sidebar done

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -10,6 +10,49 @@ export class SidebarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.onHover();
+  }
+
+  onHover(){
+    var tab = document.querySelectorAll('.__sidebar > div');
+    var sidebar = <HTMLElement>document.querySelector('.__sidebar');
+    var title  = document.querySelectorAll('.title');
+
+  tab.forEach(x => {
+      x.addEventListener('mouseover', function(){
+        (<HTMLElement>x).classList.add('titleIsActive');
+       /* sidebar.style.width = '260px';*/
+        title.forEach(x => {
+         (<HTMLElement>x).style.visibility = 'visible'
+        });
+      });
+      x.addEventListener('click', function(){
+        tab.forEach(x =>  (<HTMLElement>x).classList.remove('titleIsFocused'));
+        (<HTMLElement>x).classList.add('titleIsFocused');
+      });
+
+
+    }); 
+   sidebar.addEventListener('mouseover', function(){
+      sidebar.style.width = '260px';
+      title.forEach(x => {
+        (<HTMLElement>x).style.visibility = 'visible'
+       });
+    }) 
+    sidebar.addEventListener('mouseout', function(){
+      sidebar.style.width = '71px';
+      tab.forEach(x =>  (<HTMLElement>x).classList.remove('titleIsActive'));
+      title.forEach(x => {
+        (<HTMLElement>x).style.visibility = 'hidden'
+       });
+   
+    })
+    
+
+  
+   
+     // title.style.visibility = 'hidden';
+   
   }
 
 }
